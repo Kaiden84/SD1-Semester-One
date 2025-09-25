e4127dba11e6f06cd257d56622b4a4aeb3104c65
Kaiden84 <jackiswift1@gmail.com>
2025-09-25 10:35:23 -0600

    Created Array Test for assignment 2.1

--- ArrayTest.java
+++ ArrayTest.java
@@ -1,16 +1,22 @@
 // Written by Kaiden Stephen A. Swift on 9-25-25
 // Made for the purpose of counting to a certain interval
-public class CountingLoop {
+public class ArrayTest {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i=1;i<=100; i++) {
-        System.out.print(i + ":    ");
-          if (i%2==0) {
+        int[] numbers = new int[100];
+
+for (int i=0; i<100; i++) {
+    numbers [i] = i+1;
+}
+
+        for (int i=0; i<numbers.length; i++) {
+        System.out.print(numbers [i] + ":    ");
+          if (numbers [i] %2==0) {
             System.out.println("This is an even number");
         } else {
                       System.out.println("This is an odd number");
             }
-        sum=sum+i;
+        sum=sum+numbers [i];
         }
         System.out.println("The Total is "+sum);
  
