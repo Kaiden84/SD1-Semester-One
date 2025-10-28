eb56cc5987f77004f37c6c56056fed1e5aaa9a86
Kaiden84 <jackiswift1@gmail.com>
2025-10-28 09:33:39 -0600

    Made Horse File

---
+++ HorseWrapper.java
@@ -0,0 +1,14 @@
+public class HorseWrapper {
+   public static void main(String[] args) {
+
+        String farm = "Dr. Farmer's Farmy Farm";
+        Horse Horse1 = new Horse("Horsey", 2018);
+        Horse Horse2 = new Horse("Linda", 2019);
+        Horse Horse3 = new Horse("Bob", 1972);
+        System.out.println(Horse1);
+        System.out.println(Horse2);
+        System.out.println(Horse3);
+        System.out.println("They Lived in a farm called " + farm);
+
+   }
+}
